e1e3158b6c5ce353a579d314a42c3124ca94630f
quiteria-cx <173354745+quiteria-cx@users.noreply.github.com>
2025-02-13 16:03:48 +0000

    Update build.gradle.kts

--- gradle-springboot-kotlin-master/gradle-springboot-kotlin-master/build.gradle.kts
+++ gradle-springboot-kotlin-master/gradle-springboot-kotlin-master/build.gradle.kts
@@ -62,7 +62,7 @@ subprojects {
 			}
 		}
 		// Use JUnit Jupiter API for testing
-		testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
+		testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
 		// Use JUnit Jupiter Engine for testing
 		testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 		// Use Logback when running unit tests
